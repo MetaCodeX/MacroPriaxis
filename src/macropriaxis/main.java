46ab32d2767ee47558ea3ecab810254c5fbc3afd
MetaCodeX <carlosjuarex@outlook.es>
2025-03-09 18:25:15 -0600

    SplashScreen

--- src/macropriaxis/main.java
+++ src/macropriaxis/main.java
@@ -1,5 +1,8 @@
 package macropriaxis;
 
+import macropriaxis.util.SplashScreen;
+import javax.swing.Timer;
+
 /**
  *
  * @author MetaCodeX
@@ -9,9 +12,29 @@ package macropriaxis;
 public class main {
 
     public static void main(String[] args) {
-       
-        java.awt.EventQueue.invokeLater(() -> {
-            new IndexGigas().setVisible(true);
+        // Mostrar el SplashScreen
+        SplashScreen splash = new SplashScreen();
+        splash.setVisible(true);
+        
+        // Simular proceso de carga
+        Timer progressTimer = new Timer(30, null);
+        final int[] progress = {0};
+        
+        progressTimer.addActionListener(e -> {
+            progress[0] += 1;
+            splash.setProgress(progress[0]);
+            
+            if (progress[0] >= 100) {
+                progressTimer.stop();
+                splash.dispose();
+                
+                // Mostrar la ventana principal
+                java.awt.EventQueue.invokeLater(() -> {
+                    new IndexGigas().setVisible(true);
+                });
+            }
         });
+        
+        progressTimer.start();
     }
 }   
