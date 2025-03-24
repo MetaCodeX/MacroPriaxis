53bfaa1b83858f6803ff0e5fae0797b01ef09e53
MetaCodeX <carlosjuarex@outlook.es>
2025-03-24 11:19:15 -0600

    mejoras señores, mejoras

--- src/macropriaxis/db/DatabaseConnection.java
+++ src/macropriaxis/db/DatabaseConnection.java
@@ -4,11 +4,20 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-   
+   /*
     private static final String DB_URL = "jdbc:mysql://bcpnowq7pabjlbbgb7wd-mysql.services.clever-cloud.com:3306/bcpnowq7pabjlbbgb7wd";
     private static final String USER = "uy0cgyxczquif8jz";
     private static final String PASSWORD = "7vC6uk6kVnV1ylHG0lrV"; 
+    */
+        private static final String DB_URL = "jdbc:mysql://10.50.10.71:3306/FAREWELL";
+    private static final String USER = "MetaCodeX";
+    private static final String PASSWORD = "H4NZC0D3X";
     
+    /*
+    private static final String DB_URL = "jdbc:mysql://192.168.10.102:3306/mi_base_datos";
+    private static final String USER = "MetaCodeX";
+    private static final String PASSWORD = "H4NZC0D3X";
+    */
     public static Connection getConnection() throws SQLException {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
